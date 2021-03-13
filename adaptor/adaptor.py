7b69d3db7df5461d90f02c5fd796ad7c39d57015
Leonardo Comandini <leo@leocomandini.org>
2021-03-13 21:06:00 +0100

    Add Adaptor Schnorr

--- adaptor/adaptor.py
+++ adaptor/adaptor.py
@@ -13,6 +13,10 @@ from adaptor.ecc import (
 from adaptor.ecdsa import (
     ECDSASig,
 )
+from adaptor.schnorr import (
+    SchnorrSig,
+    schnorr_challenge,
+)
 from adaptor.dleq import (
     dleq_prove,
     dleq_verify,
@@ -66,3 +70,34 @@ def ecdsa_adaptor_recover(Y: Pubkey, a: ECDSAAdaptorSig, sig: ECDSASig) -> int:
     if Y_implied == -Y:
         return n - y
     assert False
+
+SchnorrAdaptorSig = Tuple[int, Pubkey]
+
+def schnorr_nonce(x: int, Y: Pubkey, message_hash: bytes) -> int:
+    # TODO: use right one to match test vectors
+    h = tagged_hash("Schnorr ADAPTOR", bytes_from_int(x) + Y.to_bytes() + message_hash)
+    return int_from_bytes(h) % n
+
+def schnorr_adaptor_encrypt(x: int, y: int, message_hash: bytes) -> SchnorrAdaptorSig:
+    Y = y * G
+    k = schnorr_nonce(x, Y, message_hash)
+    R = k * G
+    X = x * G
+    e = schnorr_challenge(X, R, message_hash)
+    s_a = (k + y + e * x) % n
+    return s_a, R
+
+def schnorr_adaptor_verify(X: Pubkey, Y: Pubkey, message_hash: bytes, a: SchnorrAdaptorSig) -> bool:
+    s_a, R = a
+    e = schnorr_challenge(X, R, message_hash)
+    return s_a * G == R + Y + e * X
+
+def schnorr_adaptor_decrypt(a: SchnorrAdaptorSig, y: int) -> SchnorrSig:
+    s_a, R = a
+    return (s_a - y) % n, R
+
+def schnorr_adaptor_recover(Y: Pubkey, a: SchnorrAdaptorSig, sig: SchnorrSig) -> int:
+    s_a, R_a = a
+    s, R = sig
+    assert R_a == R
+    return (s_a - s) % n
--- adaptor/tests/test_adaptor.py
+++ adaptor/tests/test_adaptor.py
@@ -4,15 +4,26 @@ from adaptor.adaptor import *
 
 class TestsAdaptor(unittest.TestCase):
 
-    def test_adaptor(self):
+    def test_adaptor_ecdsa(self):
         x = 10
         y = 14
         message_hash = b'\xaa'*32
         Y = y * G
         X = x * G
-        R = x * Y
         a = ecdsa_adaptor_encrypt(x, Y, message_hash)
         self.assertTrue(ecdsa_adaptor_verify(X, Y, message_hash, a))
         sig = ecdsa_adaptor_decrypt(a, y)
         y_recovered = ecdsa_adaptor_recover(Y, a, sig)
+        self.assertEqual(y, y_recovered)
+
+    def test_adaptor_schnorr(self):
+        x = 10
+        y = 14
+        message_hash = b'\xaa'*32
+        Y = y * G
+        X = x * G
+        a = schnorr_adaptor_encrypt(x, y, message_hash)
+        self.assertTrue(schnorr_adaptor_verify(X, Y, message_hash, a))
+        sig = schnorr_adaptor_decrypt(a, y)
+        y_recovered = schnorr_adaptor_recover(Y, a, sig)
         self.assertEqual(y, y_recovered)
